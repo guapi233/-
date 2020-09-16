b2a2e92a3613df2452ac8987f4af560e9b943f66
sanSkiller <1290148953@qq.com>
2020-09-16 16:56:02 +0800

    updated:首页-分类导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,22 +2,40 @@
 //获取应用实例
 const app = getApp()
 
-import { request } from "../../request/index.js";
+import {
+  request
+} from "../../request/index.js";
 
 Page({
   data: {
     // 轮播图数组
-    swiperList: []
+    swiperList: [],
+    // 分类列表
+    cateList: []
   },
   onLoad: async function () {
     // 获取轮播图数据
+    await this.getSwiperList();
+    // 获取分类数据
+    await this.getCateList();
+
+  },
+  // 获取轮播方法
+  async getSwiperList() {
     let result = await request({
       url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
     });
     this.setData({
       swiperList: result.data.message
     });
-
-    
+  },
+  // 获取轮播方法
+  async getCateList() {
+    let result = await request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    });
+    this.setData({
+      cateList: result.data.message
+    });
   }
 })
