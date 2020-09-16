353f3747e55848eb6dd3cc5e123321b46ff26bc3
sanSkiller <1290148953@qq.com>
2020-09-16 16:28:37 +0800

    updated:轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,18 @@ const app = getApp()
 
 Page({
   data: {
-
+    // 轮播图数组
+    swiperList: []
   },
   onLoad: function () {
-
+    // 获取轮播图数据
+    wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result) => {
+        this.setData({
+          swiperList: result.data.message
+        })
+      }
+    })
   }
 })
