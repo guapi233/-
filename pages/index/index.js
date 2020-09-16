bc36e910c446dc04355626a9ad82dd8beb993b26
sanSkiller <1290148953@qq.com>
2020-09-16 16:45:13 +0800

    updated:封装request请求

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,20 +2,22 @@
 //获取应用实例
 const app = getApp()
 
+import { request } from "../../request/index.js";
+
 Page({
   data: {
     // 轮播图数组
     swiperList: []
   },
-  onLoad: function () {
+  onLoad: async function () {
     // 获取轮播图数据
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result) => {
-        this.setData({
-          swiperList: result.data.message
-        })
-      }
-    })
+    let result = await request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    });
+    this.setData({
+      swiperList: result.data.message
+    });
+
+    
   }
 })
