c5cc3554cbcf1b6acbdde1c31766e31f97a6a2c3
sanSkiller <1290148953@qq.com>
2020-09-19 14:52:57 +0800

    updated:商品详情页-获取数据

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,18 +1,28 @@
 // pages/goods_detail/goods_detail.js
+
+import { request } from "../../request/index.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsDetail: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.getGoodsDetail(options.goods_id);
+  },
 
+  // 获取商品详情数据
+  async getGoodsDetail(goods_id) {
+    const result = await request({ url: "/goods/detail", data: { goods_id } });
+
+    this.setData({ goodsDetail: result.data.message });
   },
 
   /**
