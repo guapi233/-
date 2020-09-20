fb903e6af40c248407b54421245b0283a198296d
sanSkiller <1290148953@qq.com>
2020-09-20 14:53:51 +0800

    updated:支付-数据展示

--- pages/pay/pay.js
+++ pages/pay/pay.js
@@ -1,11 +1,17 @@
 // pages/pay/pay.js
+
+import { getSetting, chooseAddress, openSetting, showModal, showToast } from "../../utils/asyncWx.js";
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    address: {},
+    cart: [],
+    totalPrice: 0,
+    totalNum: 0
   },
 
   /**
@@ -26,9 +32,26 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    // 设置收货信息
+    this.setData({ address: wx.getStorageSync('address') });
+    // 过滤并设置购物车信息
+    this.setCart(wx.getStorageSync('cart').filter(v => v.checked) || []);
   },
 
+  // 设置购物车状态 同时计算全选、总数量价格
+  setCart(cart) {
+    this.setData({ cart });
+    wx.setStorageSync('cart', cart);
+
+    // 计算总价格、总数量（onShow 中已经过滤过了）
+    let totalPrice = 0, totalNum = 0;
+    this.data.cart.forEach(item => {
+      totalPrice += item.num * item.goods_price;
+      totalNum += item.num;
+    })
+    this.setData({ totalPrice, totalNum });
+  },
+ 
   /**
    * 生命周期函数--监听页面隐藏
    */
