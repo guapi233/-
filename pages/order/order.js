e0ec5878bc02507a9c587e94382c868568c58643
sanSkiller <1290148953@qq.com>
2020-09-20 17:34:24 +0800

    updated:个人页面-引入Tabs组件

--- pages/order/order.js
+++ pages/order/order.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        value: "全部",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "待付款",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "待发货",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "退款/退货",
+        isActive: false
+      }
+    ],
   },
 
   /**
@@ -13,6 +34,15 @@ Page({
    */
   onLoad: function (options) {
 
+  },
+
+  // Tabs change事件处理
+  handleTabsItemChange(e) {
+    const { index } = e.detail;
+    this.setData({ tabs: this.data.tabs.map(v => {
+      v.id === index ?v.isActive = true :v.isActive = false;
+      return v;
+    }) })
   },
 
   /**
