01cc0299c04ebeccb2c7ec413d9973648ad0b056
sanSkiller <1290148953@qq.com>
2020-09-20 19:59:06 +0800

    updated:商品收藏页-动态渲染

--- pages/collect/collect.js
+++ pages/collect/collect.js
@@ -5,7 +5,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+    collectList: [],
+    tabs: [
+      {
+        id: 0,
+        value: "商品收藏",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "品牌收藏",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "店铺收藏",
+        isActive: false
+      },
+      {
+        id: 3,
+        value: "浏览足迹",
+        isActive: false
+      }
+    ],
+  },
 
+  // Tabs change事件处理
+  handleTabsItemChange(e) {
+    const { index } = e.detail;
+
+    this.changeTitleByIndex(index);
+  },
+
+  // 根据标题索引来激活选中 标题数组
+  changeTitleByIndex(index) {
+    this.setData({ tabs: this.data.tabs.map(v => {
+      v.id == index ?v.isActive = true :v.isActive = false;
+      return v;
+    }) })
+  },
+
+  getCollectList() {
+    const collectList = wx.getStorageSync('collect') || [];
+
+    this.setData({ collectList })
   },
 
   /**
@@ -26,7 +69,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.getCollectList();
   },
 
   /**
